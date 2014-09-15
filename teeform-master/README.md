95b00df200f665b99335a93e43e54176a9009084
wmak <william.mak@hotmail.ca>
2014-09-15 09:37:34 -0400

    adding teeform

---
+++ teeform-master/README.md
@@ -0,0 +1,6 @@
+teeform
+=======
+
+A lightweight mobile development framework.
+
+In its current form, this framework only includes CSS and some very basic JS functionality. I hope to expand on this as I have more time to work on it in the future.
---
+++ teeform-master/js/main.js
@@ -0,0 +1,106 @@
+$(document).ready(appReady);
+
+function appReady() {
+
+	// Menu Snapper
+	var snapper = new Snap({
+		element: document.getElementById("content"),
+		disable: "right"
+	});
+
+	$(document).on("click", "#toolbar-menu", function(){
+		snapper.open("left")
+	});
+
+	snapper.on("animated", function(){
+		$("body").width("0%").width("100%"); /* Partial fix for issue #002 */
+	});
+
+	// Page Changing
+	var currPage = "";
+	var pageStack = new Array();
+
+	$(document).on("touchstart", ".link", function(event){
+		event.stopPropagation();
+		event.preventDefault();
+		if(event.handled !== true) {
+			changePage($(this).data("page"))
+		event.handled = true;
+		} else {
+			return false;
+		}
+	})
+
+	function changePage (page) {
+
+		snapper.close();
+
+		// Parsing page request
+		if (page == "back") {
+			page = lastPage.pop()
+		}
+
+		// Switching page
+		$(".page").each(function(){
+			if ($(this).data("page") == page) {
+
+				// Header hiding
+				if ($(this).hasClass("noheader")) {
+					$("#toolbar").hide()
+				} else {
+					$("#toolbar").show()
+				}
+
+				// Page animations
+				if (page == "login"){
+					$(this).slideDown();
+				} else {
+					$(this).show()
+				}
+			} else {
+
+				// Page animations
+				if (currPage == "login"){
+					$(this).slideUp()
+				} else {
+					$(this).hide()
+				}
+			}
+		});
+
+		// Special page cases
+		switch(page){
+			case "login":
+				snapper.disable();
+				break;
+			default:
+				break;
+		}
+
+		currPage = page;
+
+	}
+	changePage("login");
+
+
+	// Authentication
+	$(document).on("touchstart", ".btn.login", login);
+	$('[data-page="login"] input').bind('keypress', function(e) {
+		if(e.keyCode==13){
+			login();
+		}
+	});
+
+	function login(){
+		// TODO oauth authentication.
+		document.activeElement.blur();
+		$(".btn.login i").show();
+
+		// on success
+		changePage("notifs");
+
+		$(".btn.login i").hide();
+		snapper.enable();
+	}
+
+}
